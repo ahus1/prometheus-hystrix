4d5acd39e59a8726397076d2f6da017ef13963db
Rob van Dalen <rob.vandalen@kadaster.nl>
2017-08-24 13:24:24 +0200

    Splitted initialize in individual methods. Added functionality for extending the classes and overriding methods.

--- src/main/java/com/soundcloud/prometheus/hystrix/HystrixPrometheusMetricsPublisher.java
+++ src/main/java/com/soundcloud/prometheus/hystrix/HystrixPrometheusMetricsPublisher.java
@@ -1,19 +1,26 @@
 /**
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package com.soundcloud.prometheus.hystrix;
 
-import com.netflix.hystrix.*;
+import com.netflix.hystrix.HystrixCircuitBreaker;
+import com.netflix.hystrix.HystrixCollapserKey;
+import com.netflix.hystrix.HystrixCollapserMetrics;
+import com.netflix.hystrix.HystrixCollapserProperties;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.HystrixCommandKey;
+import com.netflix.hystrix.HystrixCommandMetrics;
+import com.netflix.hystrix.HystrixCommandProperties;
+import com.netflix.hystrix.HystrixThreadPoolKey;
+import com.netflix.hystrix.HystrixThreadPoolMetrics;
+import com.netflix.hystrix.HystrixThreadPoolProperties;
 import com.netflix.hystrix.strategy.HystrixPlugins;
 import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
 import com.netflix.hystrix.strategy.eventnotifier.HystrixEventNotifier;
@@ -26,8 +33,8 @@ import com.netflix.hystrix.strategy.properties.HystrixPropertiesStrategy;
 import io.prometheus.client.CollectorRegistry;
 
 /**
- * Implementation of a {@link HystrixMetricsPublisher} for Prometheus Metrics.
- * See <a href="https://github.com/Netflix/Hystrix/wiki/Metrics-and-Monitoring">Hystrix Metrics and Monitoring</a>.
+ * Implementation of a {@link HystrixMetricsPublisher} for Prometheus Metrics. See <a
+ * href="https://github.com/Netflix/Hystrix/wiki/Metrics-and-Monitoring">Hystrix Metrics and Monitoring</a>.
  */
 public class HystrixPrometheusMetricsPublisher extends HystrixMetricsPublisher {
 
@@ -37,8 +44,8 @@ public class HystrixPrometheusMetricsPublisher extends HystrixMetricsPublisher {
     private final boolean exportDeprecatedMetrics;
 
     public HystrixPrometheusMetricsPublisher(String namespace, CollectorRegistry registry, boolean exportProperties,
-                                             boolean exportDeprecatedMetrics,
-                                             HystrixMetricsPublisher metricsPublisherDelegate) {
+        boolean exportDeprecatedMetrics,
+        HystrixMetricsPublisher metricsPublisherDelegate) {
         this.collector = new HystrixMetricsCollector(namespace).register(registry);
         this.exportProperties = exportProperties;
         this.exportDeprecatedMetrics = exportDeprecatedMetrics;
@@ -47,44 +54,43 @@ public class HystrixPrometheusMetricsPublisher extends HystrixMetricsPublisher {
 
     @Override
     public HystrixMetricsPublisherCommand getMetricsPublisherForCommand(
-            HystrixCommandKey commandKey, HystrixCommandGroupKey commandGroupKey,
-            HystrixCommandMetrics metrics, HystrixCircuitBreaker circuitBreaker,
-            HystrixCommandProperties properties) {
+        HystrixCommandKey commandKey, HystrixCommandGroupKey commandGroupKey,
+        HystrixCommandMetrics metrics, HystrixCircuitBreaker circuitBreaker,
+        HystrixCommandProperties properties) {
         HystrixMetricsPublisherCommand delegate = metricsPublisherDelegate.getMetricsPublisherForCommand(commandKey,
-                commandGroupKey, metrics, circuitBreaker, properties);
+            commandGroupKey, metrics, circuitBreaker, properties);
 
         return new HystrixPrometheusMetricsPublisherCommand(
-                collector, commandKey, commandGroupKey, metrics, circuitBreaker, properties, exportProperties,
-                exportDeprecatedMetrics, delegate);
+            collector, commandKey, commandGroupKey, metrics, circuitBreaker, properties, exportProperties,
+            exportDeprecatedMetrics, delegate);
     }
 
     @Override
     public HystrixMetricsPublisherThreadPool getMetricsPublisherForThreadPool(
-            HystrixThreadPoolKey threadPoolKey, HystrixThreadPoolMetrics metrics,
-            HystrixThreadPoolProperties properties) {
+        HystrixThreadPoolKey threadPoolKey, HystrixThreadPoolMetrics metrics,
+        HystrixThreadPoolProperties properties) {
         HystrixMetricsPublisherThreadPool delegate = metricsPublisherDelegate.getMetricsPublisherForThreadPool(
-                threadPoolKey, metrics, properties);
+            threadPoolKey, metrics, properties);
 
         return new HystrixPrometheusMetricsPublisherThreadPool(
-                collector, threadPoolKey, metrics, properties, exportProperties, delegate);
+            collector, threadPoolKey, metrics, properties, exportProperties, delegate);
     }
 
     @Override
     public HystrixMetricsPublisherCollapser getMetricsPublisherForCollapser(
-            HystrixCollapserKey collapserKey, HystrixCollapserMetrics metrics,
-            HystrixCollapserProperties properties) {
+        HystrixCollapserKey collapserKey, HystrixCollapserMetrics metrics,
+        HystrixCollapserProperties properties) {
         HystrixMetricsPublisherCollapser delegate = metricsPublisherDelegate.getMetricsPublisherForCollapser(
-                collapserKey, metrics, properties);
+            collapserKey, metrics, properties);
 
         return new HystrixPrometheusMetricsPublisherCollapser(
-                collector, collapserKey, metrics, properties, exportProperties, delegate);
+            collector, collapserKey, metrics, properties, exportProperties, delegate);
     }
 
     /**
-     * Register an instance of this publisher, without a namespace, with the
-     * {@link com.netflix.hystrix.strategy.HystrixPlugins} singleton. The publisher
-     * registered by this method will register metrics with the default CollectorRegistry
-     * and will NOT attempt to export properties.
+     * Register an instance of this publisher, without a namespace, with the {@link
+     * com.netflix.hystrix.strategy.HystrixPlugins} singleton. The publisher registered by this method will register
+     * metrics with the default CollectorRegistry and will NOT attempt to export properties.
      *
      * @see CollectorRegistry#defaultRegistry
      */
@@ -93,19 +99,18 @@ public class HystrixPrometheusMetricsPublisher extends HystrixMetricsPublisher {
     }
 
     /**
-     * Register an instance of this publisher, without a namespace, with the
-     * {@link com.netflix.hystrix.strategy.HystrixPlugins} singleton. The publisher
-     * registered by this method will NOT attempt to export properties.
+     * Register an instance of this publisher, without a namespace, with the {@link
+     * com.netflix.hystrix.strategy.HystrixPlugins} singleton. The publisher registered by this method will NOT attempt
+     * to export properties.
      */
     public static void register(CollectorRegistry registry) {
         register(null, registry, false, true);
     }
 
     /**
-     * Register an instance of this publisher, for the given namespace, with the
-     * {@link com.netflix.hystrix.strategy.HystrixPlugins} singleton. The publisher
-     * registered by this method will register metrics with the default CollectorRegistry
-     * and will NOT attempt to export properties.
+     * Register an instance of this publisher, for the given namespace, with the {@link
+     * com.netflix.hystrix.strategy.HystrixPlugins} singleton. The publisher registered by this method will register
+     * metrics with the default CollectorRegistry and will NOT attempt to export properties.
      *
      * @see CollectorRegistry#defaultRegistry
      */
@@ -114,37 +119,36 @@ public class HystrixPrometheusMetricsPublisher extends HystrixMetricsPublisher {
     }
 
     /**
-     * Register an instance of this publisher, for the given namespace, with the
-     * {@link com.netflix.hystrix.strategy.HystrixPlugins} singleton. The publisher
-     * registered by this method will NOT attempt to export properties.
+     * Register an instance of this publisher, for the given namespace, with the {@link
+     * com.netflix.hystrix.strategy.HystrixPlugins} singleton. The publisher registered by this method will NOT attempt
+     * to export properties.
      */
     public static void register(String namespace, CollectorRegistry registry) {
         register(namespace, registry, false, true);
     }
 
     /**
-     * Register an instance of this publisher, for the given namespace, with the
-     * {@link com.netflix.hystrix.strategy.HystrixPlugins} singleton.
+     * Register an instance of this publisher, for the given namespace, with the {@link
+     * com.netflix.hystrix.strategy.HystrixPlugins} singleton.
      */
     public static void register(String namespace, CollectorRegistry registry, boolean exportProperties,
-                                boolean exportDeprecatedMetrics) {
-
+        boolean exportDeprecatedMetrics) {
         // memorize the registered plugins
         HystrixCommandExecutionHook commandExecutionHook = HystrixPlugins
-                .getInstance().getCommandExecutionHook();
+            .getInstance().getCommandExecutionHook();
         HystrixEventNotifier eventNotifier = HystrixPlugins.getInstance()
-                .getEventNotifier();
+            .getEventNotifier();
         HystrixMetricsPublisher metricsPublisher = HystrixPlugins.getInstance()
-                .getMetricsPublisher();
+            .getMetricsPublisher();
         HystrixPropertiesStrategy propertiesStrategy = HystrixPlugins.getInstance()
-                .getPropertiesStrategy();
+            .getPropertiesStrategy();
         HystrixConcurrencyStrategy concurrencyStrategy = HystrixPlugins.getInstance()
-                .getConcurrencyStrategy();
+            .getConcurrencyStrategy();
 
         // wrap the metrics publisher plugin
         HystrixPrometheusMetricsPublisher wrappedMetricsPublisher =
-                new HystrixPrometheusMetricsPublisher(namespace, registry, exportProperties,
-                        exportDeprecatedMetrics, metricsPublisher);
+            new HystrixPrometheusMetricsPublisher(namespace, registry, exportProperties,
+                exportDeprecatedMetrics, metricsPublisher);
 
         // reset all plugins
         HystrixPlugins.reset();
--- src/main/java/com/soundcloud/prometheus/hystrix/HystrixPrometheusMetricsPublisherCollapser.java
+++ src/main/java/com/soundcloud/prometheus/hystrix/HystrixPrometheusMetricsPublisherCollapser.java
@@ -1,15 +1,12 @@
 /**
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package com.soundcloud.prometheus.hystrix;
 
@@ -18,14 +15,13 @@ import com.netflix.hystrix.HystrixCollapserMetrics;
 import com.netflix.hystrix.HystrixCollapserProperties;
 import com.netflix.hystrix.strategy.metrics.HystrixMetricsPublisherCollapser;
 import com.netflix.hystrix.util.HystrixRollingNumberEvent;
-
 import java.util.Collections;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
 /**
- * Implementation of a {@link HystrixMetricsPublisherCollapser} for Prometheus Metrics.
- * See <a href="https://github.com/Netflix/Hystrix/wiki/Metrics-and-Monitoring">Hystrix Metrics and Monitoring</a>.
+ * Implementation of a {@link HystrixMetricsPublisherCollapser} for Prometheus Metrics. See <a
+ * href="https://github.com/Netflix/Hystrix/wiki/Metrics-and-Monitoring">Hystrix Metrics and Monitoring</a>.
  */
 public class HystrixPrometheusMetricsPublisherCollapser implements HystrixMetricsPublisherCollapser {
 
@@ -38,41 +34,46 @@ public class HystrixPrometheusMetricsPublisherCollapser implements HystrixMetric
     private final HystrixMetricsCollector collector;
 
     public HystrixPrometheusMetricsPublisherCollapser(
-            HystrixMetricsCollector collector, HystrixCollapserKey key, HystrixCollapserMetrics metrics,
-            HystrixCollapserProperties properties, boolean exportProperties,
-            HystrixMetricsPublisherCollapser delegate) {
-
+        HystrixMetricsCollector collector, HystrixCollapserKey key, HystrixCollapserMetrics metrics,
+        HystrixCollapserProperties properties, boolean exportProperties,
+        HystrixMetricsPublisherCollapser delegate) {
         this.metrics = metrics;
         this.collector = collector;
         this.properties = properties;
         this.exportProperties = exportProperties;
-        this.labels = Collections.singletonMap("collapser_name", key.name());
+        this.labels = prepareLabels(key);
         this.delegate = delegate;
+    }
+
+    protected Map<String, String> prepareLabels(HystrixCollapserKey key) {
+        return Collections.singletonMap(HystrixMetricsConstants.COLLAPSER_NAME, key.name());
     }
 
     @Override
     public void initialize() {
         delegate.initialize();
 
-        createCumulativeCountForEvent("count_batches", HystrixRollingNumberEvent.COLLAPSER_BATCH);
-        createCumulativeCountForEvent("count_requests_batched", HystrixRollingNumberEvent.COLLAPSER_REQUEST_BATCHED);
-        createCumulativeCountForEvent("count_responses_from_cache", HystrixRollingNumberEvent.RESPONSE_FROM_CACHE);
+        createCountersForCumulativeEvents();
+        createCountersForRollingEvents();
+        createCountersForBatchSizeEvents("Collapser the batch size metric.");
+        createCountersForShardSizeEvents("Collapser shard size metric.");
+        if (exportProperties) {
+            createCountersForPropertyEvents("Configuration property partitioned by collapser_name.");
+        }
+    }
 
-        createRollingCountForEvent("rolling_batches", HystrixRollingNumberEvent.COLLAPSER_BATCH);
-        createRollingCountForEvent("rolling_requests_batched", HystrixRollingNumberEvent.COLLAPSER_REQUEST_BATCHED);
-        createRollingCountForEvent("rolling_count_responses_from_cache", HystrixRollingNumberEvent.RESPONSE_FROM_CACHE);
+    protected void createCountersForPropertyEvents(String doc) {
+        addGauge("property_value_max_requests_in_batch", doc, () -> properties.maxRequestsInBatch().get());
 
-        String batchDoc = "Collapser the batch size metric.";
-        addGauge("batch_size_mean", batchDoc, metrics::getBatchSizeMean);
+        addGauge("property_value_request_cache_enabled", doc, () -> properties.requestCacheEnabled().get() ? 1 : 0);
 
-        createBatchSizePercentile("batch_size_percentile_25", 25, batchDoc);
-        createBatchSizePercentile("batch_size_percentile_50", 50, batchDoc);
-        createBatchSizePercentile("batch_size_percentile_75", 75, batchDoc);
-        createBatchSizePercentile("batch_size_percentile_90", 90, batchDoc);
-        createBatchSizePercentile("batch_size_percentile_99", 99, batchDoc);
-        createBatchSizePercentile("batch_size_percentile_995", 99.5, batchDoc);
+        addGauge("property_value_timer_delay_in_milliseconds", doc, () -> properties.timerDelayInMilliseconds().get());
 
-        String shardDoc = "Collapser shard size metric.";
+        addGauge("property_value_rolling_statistical_window_in_milliseconds", doc,
+            () -> properties.metricsRollingStatisticalWindowInMilliseconds().get());
+    }
+
+    public void createCountersForShardSizeEvents(String shardDoc) {
         addGauge("shard_size_mean", shardDoc, metrics::getShardSizeMean);
 
         createShardSizePercentile("shard_size_percentile_25", 25, shardDoc);
@@ -81,43 +82,50 @@ public class HystrixPrometheusMetricsPublisherCollapser implements HystrixMetric
         createShardSizePercentile("shard_size_percentile_90", 90, shardDoc);
         createShardSizePercentile("shard_size_percentile_99", 99, shardDoc);
         createShardSizePercentile("shard_size_percentile_995", 99.5, shardDoc);
-
-        if (exportProperties) {
-            String doc = "Configuration property partitioned by collapser_name.";
-
-            addGauge("property_value_max_requests_in_batch", doc,
-                    () -> properties.maxRequestsInBatch().get());
-
-            addGauge("property_value_request_cache_enabled", doc,
-                    () -> properties.requestCacheEnabled().get() ? 1 : 0);
-
-            addGauge("property_value_timer_delay_in_milliseconds", doc,
-                    () -> properties.timerDelayInMilliseconds().get());
-
-            addGauge("property_value_rolling_statistical_window_in_milliseconds", doc,
-                    () -> properties.metricsRollingStatisticalWindowInMilliseconds().get());
-        }
     }
 
-    private void createCumulativeCountForEvent(String metric, final HystrixRollingNumberEvent event) {
+    public void createCountersForBatchSizeEvents(String batchDoc) {
+        addGauge("batch_size_mean", batchDoc, metrics::getBatchSizeMean);
+
+        createBatchSizePercentile("batch_size_percentile_25", 25, batchDoc);
+        createBatchSizePercentile("batch_size_percentile_50", 50, batchDoc);
+        createBatchSizePercentile("batch_size_percentile_75", 75, batchDoc);
+        createBatchSizePercentile("batch_size_percentile_90", 90, batchDoc);
+        createBatchSizePercentile("batch_size_percentile_99", 99, batchDoc);
+        createBatchSizePercentile("batch_size_percentile_995", 99.5, batchDoc);
+    }
+
+    public void createCountersForRollingEvents() {
+        createRollingCountForEvent("rolling_batches", HystrixRollingNumberEvent.COLLAPSER_BATCH);
+        createRollingCountForEvent("rolling_requests_batched", HystrixRollingNumberEvent.COLLAPSER_REQUEST_BATCHED);
+        createRollingCountForEvent("rolling_count_responses_from_cache", HystrixRollingNumberEvent.RESPONSE_FROM_CACHE);
+    }
+
+    public void createCountersForCumulativeEvents() {
+        createCumulativeCountForEvent("count_batches", HystrixRollingNumberEvent.COLLAPSER_BATCH);
+        createCumulativeCountForEvent("count_requests_batched", HystrixRollingNumberEvent.COLLAPSER_REQUEST_BATCHED);
+        createCumulativeCountForEvent("count_responses_from_cache", HystrixRollingNumberEvent.RESPONSE_FROM_CACHE);
+    }
+
+    protected void createCumulativeCountForEvent(String metric, final HystrixRollingNumberEvent event) {
         String doc = "These are cumulative counts since the start of the application.";
         addGauge(metric, doc, () -> metrics.getCumulativeCount(event));
     }
 
-    private void createRollingCountForEvent(String metric, final HystrixRollingNumberEvent event) {
+    protected void createRollingCountForEvent(String metric, final HystrixRollingNumberEvent event) {
         String doc = "These are \"point in time\" counts representing the last X seconds.";
         addGauge(metric, doc, () -> metrics.getRollingCount(event));
     }
 
-    private void createBatchSizePercentile(String metric, final double percentile, String documentation) {
+    protected void createBatchSizePercentile(String metric, final double percentile, String documentation) {
         addGauge(metric, documentation, () -> metrics.getBatchSizePercentile(percentile));
     }
 
-    private void createShardSizePercentile(String metric, final double percentile, String documentation) {
+    protected void createShardSizePercentile(String metric, final double percentile, String documentation) {
         addGauge(metric, documentation, () -> metrics.getShardSizePercentile(percentile));
     }
 
-    private void addGauge(String metric, String helpDoc, Callable<Number> value) {
-        collector.addGauge("hystrix_collapser", metric, helpDoc, labels, value);
+    protected void addGauge(String metric, String helpDoc, Callable<Number> value) {
+        collector.addGauge(HystrixMetricsConstants.HYSTRIX_COLLAPSER, metric, helpDoc, labels, value);
     }
 }
